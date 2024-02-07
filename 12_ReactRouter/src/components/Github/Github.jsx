29edefeb1e4e9b469ed52eda3560fc21632a3610
E-srinibash <ellasrinibash0@gmail.com>
2024-02-07 11:08:33 +0530

    updated Github.jsx

--- 12_ReactRouter/src/components/Github/Github.jsx
+++ 12_ReactRouter/src/components/Github/Github.jsx
@@ -14,15 +14,17 @@ function Github() {
     // }, [])
     
   return (
-    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}
-    <img src={data.avatar_url} alt="Git picture" width={300} />
-    </div>
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl flex rounded-md '>  
+    <div className="h-1/2 w-1/2 flex" >Github Repos: {data.public_repos}</div>
+    <img className="rounded-full h-1/2 w-1/2 relative" src={data.avatar_url} alt="Git picture" width={300} />
+    
+    </div> 
   )
 }
 
 export default Github
 
 export const githubInfoLoader = async () => {
-    const response = await fetch('https://api.github.com/users/hiteshchoudhary')
+    const response = await fetch('https://api.github.com/users/E-srinibash')
     return response.json()
 }
